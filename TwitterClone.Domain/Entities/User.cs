8b7770dbb18c07c5cd8ccaace992bb97ff0fbd54
Saif Shahriar <sh.saif@proton.me>
2026-08-08 22:24:17 +0600

    feat: properly add User entity

--- TwitterClone.Domain/Entities/User.cs
+++ TwitterClone.Domain/Entities/User.cs
@@ -1,3 +1,15 @@
-namespace TwitterClone.Domain.Entities {
-internal class User {}
+namespace TwitterClone.Domain.Entities;
+
+public class User(string firstName, string lastName, string userName,
+                  string email) {
+	public User() :
+	    this(string.Empty, string.Empty, string.Empty, string.Empty) {}
+
+	public Guid     Id { get; }        = Guid.NewGuid();
+	public DateTime CreatedAt { get; } = DateTime.UtcNow;
+
+	public string FirstName { get; set; } = firstName;
+	public string LastName { get; set; }  = lastName;
+	public string UserName { get; set; }  = userName;
+	public string Email { get; set; }     = email;
 }
