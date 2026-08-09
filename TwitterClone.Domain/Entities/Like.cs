950b50b4262d0d73937baa5bbeac4ae5fe913e96
Saif Shahriar <sh.saif@proton.me>
2026-08-10 01:36:09 +0600

    fix: TweetId should not create a new Id

--- TwitterClone.Domain/Entities/Like.cs
+++ TwitterClone.Domain/Entities/Like.cs
@@ -2,6 +2,6 @@ namespace TwitterClone.Domain.Entities;
 
 public class Like {
 	public Guid     Id { get; }        = Guid.NewGuid();
-	public Guid     TweetId { get; }   = Guid.NewGuid();
+	public Guid     TweetId { get; }
 	public DateTime CreatedAt { get; } = DateTime.UtcNow;
 }
