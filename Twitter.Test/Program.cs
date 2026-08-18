259b9c727081e90d15fb457b8ec564a3ad158092
Saif Shahriar <sh.saif@proton.me>
2026-08-19 01:31:21 +0600

    add Twitter.Test console app demonstrating notification polymorphism

---
+++ Twitter.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using TwitterClone.Domain.Entities;
+
+List<Notification> notifications = [
+	new LikeNotification(Guid.NewGuid(), Guid.NewGuid()),
+	new RetweetNotification(Guid.NewGuid(), Guid.NewGuid()),
+	new FollowNotification(Guid.NewGuid(), Guid.NewGuid()),
+	new MentionNotification(Guid.NewGuid(), Guid.NewGuid()),
+	new ReplyNotification(Guid.NewGuid(), Guid.NewGuid()),
+	new DirectMessageNotification(Guid.NewGuid(), Guid.NewGuid(), "Hello"),
+	new CommentNotification(Guid.NewGuid(), Guid.NewGuid()),
+	new SystemNotification(Guid.NewGuid(), "Welcome to Murmur!")
+];
+
+foreach(var notification in notifications) {
+	Console.WriteLine(notification.GetNotificationInfo());
+	Console.WriteLine(notification.GetMessage());
+	Console.WriteLine();
+}
